4d8e46ef54284f3f45b3269ef0743d32c7fd6e4a
ericcnguyen04 <ericc.win@gmail.com>
2024-02-10 14:25:28 -0500

    oof

--- client/src/Components/Welcome.jsx
+++ client/src/Components/Welcome.jsx
@@ -1,11 +1,21 @@
-export default function Welcome () {
-    return (
+import 'bulma/css/bulma.min.css';
+import React from 'react';
+// import fintech from '/'
+
+export default function Welcome() {
+  return (
+    <div style={{ backgroundImage: '', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
+
+        <div className='containerScreen'>
         <div>
-            Welcome
-            <div>
+            <img src="/fintech.jpg" className="background-img" alt="Fintech" />
+            <div className="content">
+            <div className="box">
                 <a href='/login'>Login</a>
-                <a href='/register'>Register</a>
+            </div>
             </div>
         </div>
-    )
+        </div>
+    </div>
+  );
 }

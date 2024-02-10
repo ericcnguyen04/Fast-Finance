0a0dc1f370194b5d97d9828dfd1c368e7ad59a50
ericcnguyen04 <ericc.win@gmail.com>
2024-02-10 17:53:28 -0500

    interest rates

--- client/src/Components/What-if.jsx
+++ client/src/Components/What-if.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export default function Whatif() {
-    let [currentSavings, setCurrentSavings] = useState(null);
-    let [monthlyContribution, setmonthlyContribution] = useState(null);
-    let [expectedReturn, setexpectedReturn] = useState(null);
-    let [duration, setduration] = useState(null);
+    let [currentSavings, setCurrentSavings] = useState(null); // starting balance
+    let [monthlyContribution, setmonthlyContribution] = useState(null); // monthly deposit
+    let [expectedReturn, setexpectedReturn] = useState(null); // expected return
+    let [duration, setduration] = useState(null); // duration
 
     // let [resultData, setResultData] = useState([]);
     const [resultData, setResultData] = useState([]);
@@ -13,19 +13,19 @@ export default function Whatif() {
 
     const handleInvestment = () => {
         let yearlyContribution = monthlyContribution * 12
+        let returnProfit = expectedReturn / 100
         let newData = [];
+        let TotalContribution = currentSavings
 
         for (let i = 0; i < duration; i++) {
-            // let yearlyInterest = currentSavings * (expectedReturn / 100);
-            // currentSavings += parseInt(yearlyInterest) + parseInt(yearlyContribution);
-            interestEarned = currentSavings * (expectedReturn / 100)
-
-            currentSavings
+            currentSavings = currentSavings * (1 + returnProfit) + yearlyContribution
+            TotalContribution = currentSavings + yearlyContribution
 
             newData.push(({
               setyear: i + 1,
-              savingsEndOfYear: currentSavings, // fucked up
-              yearlyInterest: yearlyInterest,
+              TotalEquity: currentSavings.toFixed(2), // fucked up
+            //   TotalInvestment: interestEarned,
+              TotalContribution: TotalContribution,
               yearlyContribution: yearlyContribution,
             }));
         }
@@ -118,8 +118,8 @@ export default function Whatif() {
                 {resultData.map((data, index) => (
                     <tr key={index}>
                         <td>{data.setyear}</td>
-                        <td>{data.savingsEndOfYear}</td>
-                        <td>{data.yearlyInterest}</td>
+                        <td>{data.TotalEquity}</td>
+                        <td>{data.TotalInvestment}</td>
                         <td>{data.yearlyContribution}</td>
                     </tr>
                 ))}

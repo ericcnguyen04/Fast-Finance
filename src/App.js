f45afc6437639bb47aec9454091faf4a648b3980
ericcnguyen04 <ericc.win@gmail.com>
2024-02-09 20:56:23 -0500

    usestate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Routes,
   Route  //Navigate
 } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+
 
 import Login from './Components/Login'
 import Navbar from './Components/Navbar'
@@ -18,6 +20,15 @@ function App() {
   // the currently logged in user will be stored up here in state
   const [currentUser, setCurrentUser] = useState(null)
 
+  const handleLogout = () => {
+    // check to see if a token exists in local storage
+    if (localStorage.getItem('jwt')) {
+      // if so, delete it
+      localStorage.removeItem('jwt')
+      // set the user in the App state to be null
+      setCurrentUser(null)
+    }
+  }
 
   return (
     <div className="App">
@@ -46,27 +57,7 @@ function App() {
               element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser} />}
             />
 
-            {/*optionally conditionally render auth locked routes */}
-            {/* 
-            <Route 
-              path="/profile" 
-                    element={currentUser ? <Profile handleLogout={handleLogout} currentUser={currentUser} setCurrentUser={setCurrentUser} /> : <Navigate to="/login" />}
-                  /> 
-            */}
-
-            {/* <Route 
-              path="/profile"
-              element={<Profile handleLogout={handleLogout} currentUser={currentUser} setCurrentUser={setCurrentUser} />}
-            /> */}
-
-            {/* copy pasta with new pages */}
-
-            <Route 
-              path="/newItem"
-              element={<NewItem currentUser={currentUser} />}
-            />
           </Routes>
-
         </div>
       </Router>
     </div>
